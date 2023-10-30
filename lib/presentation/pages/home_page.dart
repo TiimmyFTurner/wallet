def8b6c12f6ea00703897a00b02d5c690f10e797
Arman Askari <armanaskariofficial@gmail.com>
2023-10-30 16:26:13 +0330

    create homepage appbar

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext context,WidgetRef ref) {
+    return Scaffold(
+      body: const Text("data"),
+      appBar: AppBar(title: Text(AppLocalizations.of(context)!.wallet)),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => {},
+      ),
+    );
+    //throw UnimplementedError();
   }
   
 }
