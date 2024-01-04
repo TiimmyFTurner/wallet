41b17f33ab735c92ad509cacac6a08a090b4c104
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-01-04 10:50:04 +0330

    sett up shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wallet/application/state_management/shared_preferences_provider.dart';
 import 'package:wallet/infrastructure/router.dart';
 import 'package:wallet/l10n/l10n.dart';
 import 'package:wallet/presentation/pages/home_page.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+Future<void> main() async {
+  final prefs = await SharedPreferences.getInstance();
+  return runApp(
+    ProviderScope(
+      overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
