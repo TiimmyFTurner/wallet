1e4b04026490cfc1f0ecd12517b8a8fb05b35210
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-01-04 11:09:40 +0330

    fix runn app bug

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   return runApp(
     ProviderScope(
