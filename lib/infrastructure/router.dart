fa0f807b533b6b9f6277b5c5aaf401c95cbd38d3
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-01-06 13:48:33 +0330

    improve router and add edit credit card

--- lib/infrastructure/router.dart
+++ lib/infrastructure/router.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wallet/presentation/screens/add_credit_card_screen.dart';
+import 'package:wallet/presentation/screens/edit_credit_card_screen.dart';
 import 'package:wallet/presentation/screens/home_screen.dart';
 
-
 final GoRouter router = GoRouter(routes: <RouteBase>[
-  GoRoute(path: '/',builder: (BuildContext context, GoRouterState state){
-    return const HomeScreen();
-
-  },routes: <RouteBase>[
-    GoRoute(
-      path: 'addCreditCard',
-      builder: (BuildContext context, GoRouterState state) {
-        return const AddCreditCardScreen();
-      },
-    ),
-  ],)
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const HomeScreen(),
+    routes: <RouteBase>[
+      GoRoute(
+        path: 'addCreditCard',
+        builder: (context, state) => const AddCreditCardScreen(),
+      ),
+      GoRoute(
+        path: 'editCreditCard/:creditCardId',
+        builder: (context, state) =>
+            EditCreditCardScreen(state.pathParameters['creditCardId']!),
+      ),
+    ],
+  )
 ]);
