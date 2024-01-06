4c720783bae1cf9f292e24122774d7b8e0618b7c
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-01-06 19:08:03 +0330

    create note card provider

---
+++ lib/application/state_management/note_cards_provider.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:wallet/application/state_management/shared_preferences_provider.dart';
+import 'package:wallet/domain/note_card_model.dart';
+
+part 'note_cards_provider.g.dart';
+
+@riverpod
+class NoteCards extends _$NoteCards {
+  dynamic _prefs;
+
+  List<NoteCard> _fetchCards() {
+    _prefs = ref.watch(sharedPreferencesProvider);
+    final storedString = _prefs.getString('note_cards_list');
+    if (storedString != null) {
+      final individualJsons = storedString.split('|');
+      final deserializedList =
+          individualJsons.map((json) => jsonDecode(json)).toList();
+      List<NoteCard> noteCardList = [];
+      for (final item in deserializedList) {
+        NoteCard nc = NoteCard.fromJson(item);
+        noteCardList.add(nc);
+      }
+      return noteCardList;
+    }
+    return [];
+  }
+
+  void _saveSharedPreferences() {
+    if (state != []) {
+      final jsonList = state.map((card) => jsonEncode(card.toJson()));
+      final joinedString = jsonList.join('|');
+      _prefs.setString('note_cards_list', joinedString);
+    }
+  }
+
+  @override
+  List<NoteCard> build() {
+    return _fetchCards();
+  }
+
+  void addNoteCard(NoteCard noteCard) {
+    state = [...state, noteCard];
+    _saveSharedPreferences();
+  }
+
+  void removeNoteCard(String noteCardId) {
+    state = [
+      for (final card in state)
+        if (card.id != noteCardId) card,
+    ];
+    _saveSharedPreferences();
+  }
+
+  void editNoteCard(NoteCard noteCard) {
+    state = [
+      for (final card in state)
+        if (card.id == noteCard.id) noteCard else card
+    ];
+    _saveSharedPreferences();
+  }
+}
---
+++ lib/application/state_management/note_cards_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'note_cards_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$noteCardsHash() => r'faffea62e4a08fe3bdd2128e17e7af81ab92e968';
+
+/// See also [NoteCards].
+@ProviderFor(NoteCards)
+final noteCardsProvider =
+    AutoDisposeNotifierProvider<NoteCards, List<NoteCard>>.internal(
+  NoteCards.new,
+  name: r'noteCardsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$noteCardsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$NoteCards = AutoDisposeNotifier<List<NoteCard>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
