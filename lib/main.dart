2888bd12a14c31978b8ba227f139f221ad09baca
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-01-12 18:51:42 +0330

    fix dark mode defoult theme bug

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   return runApp(
@@ -28,22 +27,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final String? font = Platform.localeName.substring(0,2) == "fa" ? 'Koodak' : null;
-    final ColorScheme defaultScheme = ColorScheme.fromSeed(seedColor: Colors.lightBlue);
+    final String? font =
+        Platform.localeName.substring(0, 2) == "fa" ? 'Koodak' : null;
+    final ColorScheme defaultScheme =
+        ColorScheme.fromSeed(seedColor: Colors.lightBlue);
+    final ColorScheme defaultDarkScheme = ColorScheme.fromSeed(
+        seedColor: Colors.lightBlue, brightness: Brightness.dark);
     return DynamicColorBuilder(builder: (lightColorScheme, darkColorScheme) {
       return MaterialApp.router(
         routerConfig: router,
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: lightColorScheme ?? defaultScheme,
-          useMaterial3: true,
-          fontFamily: font
-        ),
+            colorScheme: lightColorScheme ?? defaultScheme,
+            useMaterial3: true,
+            fontFamily: font),
         darkTheme: ThemeData(
-          colorScheme: darkColorScheme ?? defaultScheme,
-          useMaterial3: true,
-          fontFamily: font
-        ),
+            colorScheme: darkColorScheme ?? defaultDarkScheme,
+            useMaterial3: true,
+            fontFamily: font),
         themeMode: ThemeMode.system,
         supportedLocales: L10n.all,
         // locale: const Locale('fa'),
