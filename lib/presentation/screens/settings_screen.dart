ea240f6d4e322965c20ed7bd9b79782dd019eb48
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2024-02-12 16:38:38 +0330

    remove backup string test variable

--- lib/presentation/screens/settings_screen.dart
+++ lib/presentation/screens/settings_screen.dart
@@ -242,7 +242,6 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
               if (result != null) {
                 File file = File(result.files.single.path!);
                 String backupString = await file.readAsString();
-                // String backupString = "Y2N8fnx7ImlkIjoiNWRmNTJmODAtYTAxOS0xZTU0LTlmMDgtZmY3NGFlZjUyOWM3IiwiYmdJZCI6IjYiLCJ0aXRsZSI6Itqp2KfYsdiqINmF2LrYp9iy2YciLCJuYW1lIjoi2LnZhNuMINmF2K3Zhdiv24wiLCJudW1iZXIiOiIxMjM0MTIzNDEyMzQxMjM0IiwiY3Z2MiI6IjEyMzQiLCJleHAiOiIxMC8wNiIsInNoYmEiOiItIiwicGFzcyI6Ii0iLCJub3RlIjoiLSIsImJhbmsiOnsibmFtZSI6Im1lbGxpIiwiY29kZSI6IjAifX18fnx7ImlkIjoiYzY1YTJlYTAtYTBhOS0xZTU0LTlmMDgtZmY3NGFlZjUyOWM3IiwiYmdJZCI6IjEiLCJ0aXRsZSI6Itm+2LMg2KfZhtiv2KfYsiIsIm5hbWUiOiLYsdi22Kcg2YXZiNiz2YjbjCIsIm51bWJlciI6IjEyMzQ1Njc4OTAxMjM0NTYiLCJjdnYyIjoiMzIxNCIsImV4cCI6IjAxLzAzIiwic2hiYSI6Ii0iLCJwYXNzIjoiLSIsIm5vdGUiOiItIiwiYmFuayI6eyJuYW1lIjoiYW5zYXIiLCJjb2RlIjoiMCJ9fXx+fnxuY3x+fHsiaWQiOiI0YWE4MzFiMC1hMzAzLTFlNTQtOWYwOC1mZjc0YWVmNTI5YzciLCJiZ0lkIjoiMyIsInRpdGxlIjoi2KLYr9ix2LMg2K7ZiNmG2Ycg2LnZhNuMIiwibm90ZSI6Itiq2YfYsdin2YYg2YXbjNiv2KfZhiDYp9mF2KfZhSDYrtuM2KfYqNin2YYg2LHYttmI24wg2b7ZhNin2qkg27HbsFxuIn0=";
                 ref
                     .read(backupStringProvider.notifier)
                     .restoreBackup(backupString);
